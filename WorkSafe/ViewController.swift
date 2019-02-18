d87d20ce64696f0c97dd21374ff092f5d054e8fc
JacobTassinari <jubrowning@Interapts-MacBook-Pro-3.local>
2019-02-18 13:30:52 -0600

    hi work

--- WorkSafe/ViewController.swift
+++ WorkSafe/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let pineColor = UIColor(red: 0/255, green:165/255, blue:114/255, alpha: 1.0)
+        let teaColor = UIColor(displayP3Red: 208/255, green: 240/255, blue: 192/255, alpha: 1.0)
+        let layer = CAGradientLayer()
+        layer.frame = view.bounds
+        layer.colors = [pineColor.cgColor, teaColor.cgColor]
+        view.layer.addSublayer(layer)
     }
     @IBOutlet weak var username_text: UITextField!
     @IBOutlet weak var password_text: UITextField!
